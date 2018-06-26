977e96c9dc96c93c3e08745ffe7011a68bc2453a
Frikker <stolpnikk.97@gmail.com>
2018-06-26 16:21:57 +0300

    fixed several errors in Task6

--- Task6.rb
+++ Task6.rb
@@ -1,5 +1,13 @@
+require './Task5.rb'
+
 class JellyBean < Dessert
   attr_accessor :flavor
+
+  def initialize(name = '', calories = 0, flavor = '')
+    @name = name
+    @calories = calories
+    @flavor = flavor
+  end
 
   def delicious?
     !@flavor.casecmp?('black lacorice')
