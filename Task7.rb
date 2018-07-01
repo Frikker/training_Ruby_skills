17cfce3d507e2f8748f786399bdd9f874ce568e9
Frikker <stolpnikk.97@gmail.com>
2018-07-02 00:01:52 +0300

    fixed some bugs and add new features

--- Task7.rb
+++ Task7.rb
@@ -1,19 +1,38 @@
 module MoneyConversation
-  DOLLAR = 32.26
-  EURO = 43.61
-  def in(arg)
-    case arg
-    when :dollars,:dollar
-      self / DOLLAR
-    when :euros, :euro
-      self / EURO
-    when :rubles, :ruble
-      self
-    else
-      raise NoSuchCurrency
+  RATE = { euro: 43.61, dollar: 32.26, ruble: 1 }
+  @@current_currency
+  def euro
+    @@current_currency = :euro
+    self
+  end
+
+  def dollar
+    @@current_currency = :dollar
+    self
+  end
+
+  def ruble
+    @@current_currency = :ruble
+    self
+  end
+
+  def in(currency)
+    currency = currency.to_s.delete('s').to_sym
+    if RATE.key?(currency)
+      if @@current_currency == currency
+        self
+      else
+        res = self * RATE[@@current_currency]
+        @@current_currency = currency
+        res / RATE[currency]
+      end
     end
   end
+  alias :euros :euro
+  alias :dollars :dollar
+  alias :rubles :ruble
 end
+
 class NoSuchCurrency < StandardError
   def message
     puts "Can't convert to your currency"
@@ -21,16 +40,8 @@ class NoSuchCurrency < StandardError
 end
 
 class Numeric
-  include MoneyConvertation
-  def dollars
-    self * DOLLAR
-  end
-
-  def euros
-    self * EURO
-  end
-
-  def rubles
-    self
-  end
+  include MoneyConversation
 end
+
+
+puts 10.dollars.in(:euro).in(:ruble)
